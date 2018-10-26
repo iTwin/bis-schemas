2f2ac52732d3f1f8b554da7ad0e66827c56709f3
Aurora.Lane <22353908+Aurora-Lane@users.noreply.github.com>
2018-10-26 11:53:00 -0400

    add beta versioning

--- tools/packages/packageSchemas.py
+++ tools/packages/packageSchemas.py
@@ -40,8 +40,21 @@ def publishPackage(packagedir, doPublish):
         exit(1);
 
 # Replace ${macros} with values in specified file
-def setMacros(packagedir, domainName, PACKAGE_VERSION = None):
+def setMacros(packagedir, domainName, PACKAGE_VERSION = None, IS_BETA = False):
     packagefile = os.path.join(packagedir, 'package.json')
+    version = PACKAGE_VERSION.lower()
+    if IS_BETA:
+        cmd = 'npm view @bentley/BisSchemas-' + domainName + ' versions'
+        try:
+            list = subprocess.check_output(cmd, shell=False)
+            betaversionlist = filter(lambda x: 'beta' in x, list)
+            if not betaversionlist:
+                raise Exception('go to beta.1')
+            betanum = float(betaversionlist[len(betaversionlist)].rsplit('.', 1)[1]) + 1
+            version = version + '-beta.' + str(betanum)
+        except:
+            version = version + '-beta.1'
+    
     str = ''
     with open(packagefile, 'r') as pf:
         str = pf.read()
@@ -49,7 +62,7 @@ def setMacros(packagedir, domainName, PACKAGE_VERSION = None):
     with open(packagefile, 'w') as pf:
         str = str.replace(r'${DOMAIN_NAME}', domainName)
         if (PACKAGE_VERSION):
-            str = str.replace(r'${PACKAGE_VERSION}', PACKAGE_VERSION.lower())
+            str = str.replace(r'${PACKAGE_VERSION}', version)
         pf.write(str)
 
 # Generate BIS Schemas packages
@@ -82,13 +95,13 @@ def generate_schema_package(outputpackagedir, parentSourceDir, domain, templateF
     dstpackagefile = os.path.join(outputpackagedir, 'package.json')
     shutil.copyfile(templateFile, dstpackagefile);
 
-    setMacros(outputpackagedir, domain, version)
+    return version
 #
 #   main
 #
 if __name__ == '__main__':
-    if len(sys.argv) < 5:
-        print "Syntax: ", sys.argv[0], " outputpackageparentdir domain sourceDir templateFile {publish|print}"
+    if len(sys.argv) < 6:
+        print "Syntax: ", sys.argv[0], " outputpackageparentdir domain sourceDir templateFile {publish|print} isBeta"
         exit(1)
     
     outdirParent = sys.argv[1]
@@ -96,6 +109,7 @@ if __name__ == '__main__':
     sourceDir = sys.argv[3]
     templateFile = sys.argv[4]
     doPublish = (sys.argv[5].lower() == 'publish');
+    beta = (sys.argv[6].lower() == 'true')
 
     if outdirParent.endswith ('/') or outdirParent.endswith ('\\'):
         outdirParent = outdirParent[0:len(outdirParent)-1]
@@ -106,7 +120,7 @@ if __name__ == '__main__':
         print '*** ' + pkgdir + ' already exists. Remove output directory before calling this script';
         exit(1)
 
-    generate_schema_package(pkgdir, sourceDir, domain, templateFile)
+    setMacros(pkgdir, domain, generate_schema_package(pkgdir, sourceDir, domain, templateFile), beta)
     publishPackage(pkgdir, doPublish)
 
     exit(0)
