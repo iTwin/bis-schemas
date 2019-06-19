45fbd8147c4932998d09af9ff5970ddb4d9abda2
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2019-06-18 21:38:57 -0400

    Clean up the python script

--- tools/packages/packageSchemas.py
+++ tools/packages/packageSchemas.py
@@ -7,8 +7,8 @@
 #--------------------------------------------------------------------------------------
 
 # This program creates a directory that looks like an npm package, naming it
-# with the specified name, and copying into it the schemas that are defined in the
-# specified domain.
+# with the specified name, and copying the schemas that are defined in the
+# specified domain folder into it.
 
 import os
 import sys
@@ -24,26 +24,11 @@ def isNewer(version1, version2):
         return str(v2[0]) + "." + str(v2[1]) + "." + str(v2[2])
     return str(v1[0]) + "." + str(v1[1]) + "." + str(v1[2])
 
-# publish a package
-def publishPackage(packagedir, doPublish):
-
-    if not doPublish:
-        print packagedir;
-        return;
-
-    pubcmd = 'npm publish '
-    pubcmd += '--@bentley:registry=https://bentley.jfrog.io/bentley/api/npm/staging/ '
-    pubcmd += packagedir
-    pubcmd += ' --dry-run'
-
-    if 0 != os.system(pubcmd):
-        exit(1);
-
 # Replace ${macros} with values in specified file
 def setMacros(packagedir, domainName, PACKAGE_VERSION = None, IS_BETA = False):
     packagefile = os.path.join(packagedir, 'package.json')
     version = PACKAGE_VERSION.lower()
-    packageName = 'bis-' + domainName.lower();
+    packageName = 'bis-' + domainName.lower()
     if IS_BETA:
         cmd = 'npm view @bentley/' + packageName + ' versions'
         try:
@@ -69,17 +54,16 @@ def setMacros(packagedir, domainName, PACKAGE_VERSION = None, IS_BETA = False):
             reader = reader.replace(r'${PACKAGE_VERSION}', version)
         pf.write(reader)
 
-# Generate BIS Schemas packages
-# @param outdirParent The path to the output package's parent directory
+# Generate BIS Schemas package 
+# @param outdir The path to the output package's parent directory
 # @param parentSourceDir The source directory, i.e., %SrcRoot%Domains
-# @param domain The domain to be packaged
+# @param domain The path to the domain that will be packaged.
 # @param templateFile The location of the package template file
-def generate_schema_package(outputpackagedir, parentSourceDir, domain, templateFile):
-
+def generate_schema_package(outDir, parentSourceDir, domain, templateFile):
     version = '1.0.0'
     schemaSourceDir = os.path.join(parentSourceDir, domain)
 
-    os.makedirs(outputpackagedir);
+    os.makedirs(outDir)
 
     # Copy schemas into place without modifying them.
     filesToCopy = []
@@ -92,38 +76,40 @@ def generate_schema_package(outputpackagedir, parentSourceDir, domain, templateF
                 filesToCopy.append(os.path.join(root, file))
 
     for fileToCopy in filesToCopy:
-        shutil.copyfile(fileToCopy, os.path.join(outputpackagedir, os.path.basename(fileToCopy)))
+        shutil.copyfile(fileToCopy, os.path.join(outDir, os.path.basename(fileToCopy)))
 
     # Generate the package.json file
-    dstpackagefile = os.path.join(outputpackagedir, 'package.json')
-    shutil.copyfile(templateFile, dstpackagefile);
+    dstpackagefile = os.path.join(outDir, 'package.json')
+    shutil.copyfile(templateFile, dstpackagefile)
 
     return version
-#
-#   main
-#
+
+# @param outDir  The output directory for the package folder
+# @param domain  The name of the domain folder to use
+# @param sourceDir  The root folder where 
+# @param templateFile  The template package.json file to use when generating the new package json.
+# @param isPrerelease  Boolean flag to determine whether to add where the package should use a pre-release flag or not.
 if __name__ == '__main__':
     if len(sys.argv) < 6:
-        print "Syntax: ", sys.argv[0], " outputpackageparentdir domain sourceDir templateFile {publish|print} isBeta"
+        print "Syntax: ", sys.argv[0], " outputDir domain sourceDir templateFile isPrerelease"
         exit(1)
-    
-    outdirParent = sys.argv[1]
+
+    outDir = sys.argv[1]
     domain = sys.argv[2]
     sourceDir = sys.argv[3]
     templateFile = sys.argv[4]
-    doPublish = (sys.argv[5].lower() == 'publish');
-    beta = (sys.argv[6].lower() == 'true')
+    prerelease = (sys.argv[6].lower() == 'true')
 
-    if outdirParent.endswith ('/') or outdirParent.endswith ('\\'):
-        outdirParent = outdirParent[0:len(outdirParent)-1]
-    
-    pkgdir = os.path.join(outdirParent, domain)
+    if outDir.endswith ('/') or outDir.endswith ('\\'):
+        outDir = outDir[0:len(outDir)-1]
+
+    pkgdir = os.path.join(outDir, domain)
 
     if os.path.exists(pkgdir):
-        print '*** ' + pkgdir + ' already exists. Remove output directory before calling this script';
+        print '*** ' + pkgdir + ' already exists. Remove output directory before calling this script'
         exit(1)
 
-    setMacros(pkgdir, domain, generate_schema_package(pkgdir, sourceDir, domain, templateFile), beta)
-    publishPackage(pkgdir, doPublish)
+    # Generates the package direction in the path provided and returns the version of the package it just created.
+    version = generate_schema_package(pkgdir, sourceDir, domain, templateFile)
 
-    exit(0)
+    setMacros(pkgdir, domain, version, prerelease)
