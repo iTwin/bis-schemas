f92b8e679e9ad551dbcb2a11f76d08d2b26bc7fc
Colin Kerr <33028649+ColinKerr@users.noreply.github.com>
2019-11-11 21:20:15 +0000

    Merged PR 53477: #205982 - Add scripts to generate and publish packages.  Update package schemas
    #205982 - Add scripts to generate and publish packages.  Update package schemas yaml script to use new scripts.  Update inventory generation to improve logging and support more flags.
    
    Related work items: #205982

--- tools/packages/generateInventory.js
+++ tools/packages/generateInventory.js
@@ -8,7 +8,7 @@
 
 "use strict";
 
-const fs = require("fs");
+const fs = require("fs-extra");
 const readdirp = require("readdirp");
 const argv = require("yargs").argv;
 
@@ -25,40 +25,48 @@ function zeroPad (digit) {
   return digit;
 }
 
-async function getReleasedSchemas() {
-  const allSchemas = await readdirp.promise(argv.BisRoot, {fileFilter: "*.ecschema.xml", directoryFilter: ["!docs", "!node_modules", "!tools", "!.vscode", "!cmaps"]});
+function createVersion(vRead, vWrite, vPatch) {
+  return zeroPad(vRead) + "." + zeroPad(vWrite) + "." + zeroPad(vPatch);
+}
 
-  const schemaJSON = {};
+async function getReleasedSchemas() {
+  const allSchemas = await readdirp.promise(argv.BisRoot, {fileFilter: "*.ecschema.xml", directoryFilter: ["!docs", "!node_modules", "!tools", "!.vscode", "!cmaps", "!packageOut"]});
+
+  // Create base inventory based on schema files in the bis-schemas repo
+  const schemaInventory = {};
   for (const entry of allSchemas) {
     const schemaInfo = {
       name: entry.basename.match(/\w+/)[0], 
       path: entry.path, 
       released: entry.path.includes("Released"), 
-      version: entry.basename.match(/\d+\.\d+\.\d+/) !== null ? entry.basename.match(/\d+\.\d+\.\d+/)[0] : ""
+      version: entry.basename.match(/\d+\.\d+\.\d+/) ? entry.basename.match(/\d+\.\d+\.\d+/)[0] : ""
     };
     
-    if (schemaJSON[schemaInfo.name] == undefined) {
-      schemaJSON[schemaInfo.name] = [];
+    if (!schemaInfo.released) schemaInfo.comment = "Working Copy";
+
+    if (schemaInventory[schemaInfo.name] == undefined) {
+      schemaInventory[schemaInfo.name] = [];
     }
-    schemaJSON[schemaInfo.name].push(schemaInfo);
+    schemaInventory[schemaInfo.name].push(schemaInfo);
   }
 
+  // Cleanup and merge information from the bis checksum wiki with information gathered from the bis-schemas repo
   const bisChecksumDump = JSON.parse (fs.readFileSync("BisChecksumDump.json"));
   for (const schemaChecksum of bisChecksumDump.schemas) {
-    let version = schemaChecksum.comment.match(/(?<write>\d+)\.(?<read>\d+)\.(?<patch>\d+)(?<comment>.*)/)
-    if (null === version) {
+    // Try to pull the version from the comment.  If no version set in comment assume it's 1.0.0
+    let version = schemaChecksum.comment.match(/(?<read>\d+)\.(?<write>\d+)\.(?<patch>\d+)(?<comment>.*)/)
+    if (!version) {
       version = "01.00.00";
     } else {
       schemaChecksum.comment = version.groups.comment;
-      version = zeroPad(version.groups.write) + "." + zeroPad(version.groups.read) + "." + zeroPad(version.groups.patch);
+      version = createVersion(version.groups.read, version.groups.write, version.groups.patch);
     }
     schemaChecksum.version = version;
-  }
 
-  for (const schemaChecksum of bisChecksumDump.schemas) {
-    const schemaInfo = schemaJSON[schemaChecksum.name];
+    // Merge checksum info from bis checksum wiki into the list of schemas in the bis-schemas repo
+    const schemaInfo = schemaInventory[schemaChecksum.name];
     if (undefined === schemaInfo || null === schemaInfo) {
-      schemaJSON[schemaChecksum.name] = [{...schemaChecksum}];
+      schemaInventory[schemaChecksum.name] = [{...schemaChecksum}];
       continue;
     }
     let checksumMerged = false;
@@ -73,19 +81,37 @@ async function getReleasedSchemas() {
     }
   }
 
-  for (const [name, schemaInfos] of Object.entries(schemaJSON)) {
+  for (const [name, schemaInfos] of Object.entries(schemaInventory)) {
     for (const schema of schemaInfos) {
-      if (schema.released) continue;
+      if (!schema.path) {
+        if (!schema.comment && schema.approved !== "Yes") schema.comment = "Known Bad";
+      continue;
+      }
 
-      if (undefined === schema.comment) schema.comment = "";
-      if (undefined === schema.sha1) schema.sha1 = "";
-      if (undefined === schema.author) schema.author = "";
-      if (undefined === schema.approved) schema.approved = "No";
-      if (undefined === schema.date) schema.date = "Unknown";
-      if (undefined === schema.dynamic) schema.dynamic = "No";
+      // Get version from inside file and verify it matches or set version in info if not set
+      const schemaXml = fs.readFileSync(schema.path).toString();
+      const versionMatch = schemaXml.match(/<ECSchema .*version="(?<read>\d+)\.(?<write>\d+)(\.(?<patch>\d+))?/);
+      if (!versionMatch || !versionMatch.groups.read || !versionMatch.groups.write) {
+        throw new Error(`Could not find version in schema xml for file ${schema.path}`);
+      }
+
+      const version = !versionMatch.groups.patch ? createVersion (versionMatch.groups.read, "0", versionMatch.groups.write) : createVersion (versionMatch.groups.read, versionMatch.groups.write, versionMatch.groups.patch);
+      if(!schema.version) {
+        schema.version = version;
+      } else if (schema.version !== version) {
+        throw new Error (`The version in the file for schema ${schema.path} ${version} doesn't match the version recorded in the inventory ${schema.version}`);
+      }
+
+      // Set base attributes
+      if (!schema.comment) schema.comment = "";
+      if (!schema.sha1) schema.sha1 = "";
+      if (!schema.author) schema.author = "";
+      if (!schema.approved) schema.approved = "No";
+      if (!schema.date) schema.date = "Unknown";
+      if (!schema.dynamic) schema.dynamic = "No";
     }
   }
-  fs.writeFileSync('SchemaInventory.json', JSON.stringify(schemaJSON, null, 2));
+  fs.writeFileSync('SchemaInventory.json', JSON.stringify(schemaInventory, null, 2));
 }
 
 getReleasedSchemas();
