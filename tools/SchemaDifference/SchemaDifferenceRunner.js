8e9d132c0e9b0e9f6c6c41612836e2d780ade9ab
Naveed Khan <38525837+naveedkhan8067@users.noreply.github.com>
2021-06-24 19:14:36 +0000

    Merged PR 171856: Copyright headers for public repo
    As bis-schemas repo is going to be public soon so proper copyrights headers are added.
    
    Related work items: #634634

--- tools/SchemaDifference/SchemaDifferenceRunner.js
+++ tools/SchemaDifference/SchemaDifferenceRunner.js
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
 // This tool is designed to validate all schemas in the bis-schema repository.
 
 "use strict";
