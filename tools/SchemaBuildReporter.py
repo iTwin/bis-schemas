7edbaddb84fce3bf89ad95b6165e6fbe39ef4a41
naveedkhan8067 <38525837+naveedkhan8067@users.noreply.github.com>
2021-11-22 11:45:17 +0100

    Update SchemaBuildReporter.py

--- tools/SchemaBuildReporter.py
+++ tools/SchemaBuildReporter.py
@@ -3,7 +3,7 @@
 #
 #     $Copyright (c) Bentley Systems, Incorporated. All rights reserved. $
 #
-#  $See LICENSE.md in the project root for license terms and full copyright notice. $
+#  $See LICENSE.md in the project root for license terms and full copyright notice.. $
 #
 #--------------------------------------------------------------------------------------
 
