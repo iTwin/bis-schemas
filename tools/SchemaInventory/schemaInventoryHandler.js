284864f05f565ad54d28b91a37810327eeda8b8a
naveedkhan8067 <38525837+naveedkhan8067@users.noreply.github.com>
2025-11-04 10:59:10 +0100

    test

--- tools/SchemaInventory/schemaInventoryHandler.js
+++ tools/SchemaInventory/schemaInventoryHandler.js
@@ -192,6 +192,7 @@ function getInventoryPath() {
 
 function getSchemaVersion(bisRootDir, schemaPath) {
   const fullPath = path.normalize(path.join(bisRootDir, schemaPath));
+  console.log(fullPath);
   const schemaXml = fs.readFileSync(fullPath).toString();
   const versionMatch = schemaXml.match(/<ECSchema .*version="(?<read>\d+)\.(?<write>\d+)(\.(?<patch>\d+))?/);
   if (!versionMatch || !versionMatch.groups.read || !versionMatch.groups.write) {
